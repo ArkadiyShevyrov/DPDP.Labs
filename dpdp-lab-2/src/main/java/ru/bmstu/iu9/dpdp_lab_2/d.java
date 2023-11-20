cc59b33f7a45f32c7c946f31c1bf9fe977e0754a
a.shevyrov <108514147+ArkadiyShevyrov@users.noreply.github.com>
2023-11-20 14:58:32 +0300

    TEMP

--- dpdp-lab-2/src/main/java/ru/bmstu/iu9/dpdp_lab_2/d.java
+++ dpdp-lab-2/src/main/java/ru/bmstu/iu9/dpdp_lab_2/d.java
@@ -1,12 +1,18 @@
 package ru.bmstu.iu9.dpdp_lab_2;
 
+import mpi.MPI;
 import ru.bmstu.iu9.dpdp_lab_2.model.*;
 import ru.bmstu.iu9.dpdp_lab_2.service.Service;
 import java.util.Arrays;
 
 public class d {
     public static void main(String[] args) {
-        Service service = new Service();
+        MPI.Init(args);
+
+        // Get the rank and size of the MPI process
+        int rank = MPI.COMM_WORLD.Rank();
+        int size = MPI.COMM_WORLD.Size();
+
         double[][] a = {
                 {2.0, 1.0, 1.0},
                 {1.0, 2.0, 1.0},
@@ -14,9 +20,9 @@ public class d {
         };
         double[] x = {0.0, 0.0, 0.0};
         double[] b = {4.0, 4.0, 4.0};
-        double[] res1 = service.solveSystem(a, x, b, new ConjugateGradient());
-        double[] res2 = service.solveSystem(a, x, b, new MinimalResiduals());
-        double[] res3 = service.solveSystem(a, x, b, new SimpleIteration());
+        double[] res1 = Service.solveParallelSystem(a, x, b, new ConjugateGradient(), rank, size);
+        double[] res2 = Service.solveParallelSystem(a, x, b, new MinimalResiduals(), rank, size);
+        double[] res3 = Service.solveParallelSystem(a, x, b, new SimpleIteration(), rank, size);
         System.out.println(Arrays.toString(res1));
         System.out.println(Arrays.toString(res2));
         System.out.println(Arrays.toString(res3));
--- dpdp-lab-2/src/main/java/ru/bmstu/iu9/dpdp_lab_2/service/Service.java
+++ dpdp-lab-2/src/main/java/ru/bmstu/iu9/dpdp_lab_2/service/Service.java
@@ -1,16 +1,46 @@
 package ru.bmstu.iu9.dpdp_lab_2.service;
 
 
+import mpi.MPI;
+import mpi.MPIException;
 import ru.bmstu.iu9.dpdp_lab_2.model.SolveSystem;
 
 public class Service {
 
 
-    public double[] solveSystem(double[][] a, double[] x, double[] b, SolveSystem solveSystem) {
+    public static double[] solveParallelSystem(double[][] A, double[] x, double[] b, SolveSystem solveSystem, int rank, int size) throws MPIException {
+        int N = b.length;
+
+        // Broadcast the size of the system to all processes
+        MPI.COMM_WORLD.Bcast(new int[]{N}, 0, 1, MPI.INT, 0);
+
+        // Scatter the matrix A among processes
+        double[][] localA = new double[N / size][N];
+        MPI.COMM_WORLD.Scatter(A, 0, N / size, MPI.OBJECT, localA, 0, N / size, MPI.OBJECT, 0);
+
+        // Broadcast vectors x and b to all processes
+        MPI.COMM_WORLD.Bcast(x, 0, N, MPI.DOUBLE, 0);
+        MPI.COMM_WORLD.Bcast(b, 0, N, MPI.DOUBLE, 0);
+
+        // Perform parallel solution
         double[] currentX = x;
-        while (solveSystem.g(a, currentX, b)) {
-            currentX = solveSystem.f(a, currentX, b);
+        while (solveSystem.g(localA, currentX, b)) {
+            currentX = solveSystem.f(localA, currentX, b);
+            // Gather updated currentX to the root process
+            MPI.COMM_WORLD.Gather(currentX, 0, N, MPI.DOUBLE, x, 0, N, MPI.DOUBLE, 0);
         }
-        return currentX;
+
+        // Gather the final result to the root process
+        MPI.COMM_WORLD.Gather(currentX, 0, N, MPI.DOUBLE, x, 0, N, MPI.DOUBLE, 0);
+
+        // Print the result on the root process
+        if (rank == 0) {
+            System.out.println("Solution:");
+            for (int i = 0; i < N; i++) {
+                System.out.println("x[" + i + "] = " + x[i]);
+            }
+        }
+
+        return x;
     }
 }
